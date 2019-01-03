43bf1197f0024523f7f2d5138b300a90399c36b7
Josh Stillman <josh.stillman@giantmachines.com>
2019-01-02 23:19:30 -0500

    connect to mongo and setup basic db access WIP

--- src/db.ts
+++ src/db.ts
@@ -1,15 +1,143 @@
-import mongoose from 'mongoose';
+import { MongoClient, MongoClientOptions, DbCollectionOptions, ReadPreference, Db } from 'mongodb';
 
-mongoose.connect('mongodb://localhost:27017/test', {useNewUrlParser: true});
+import dotenv from 'dotenv';
+dotenv.config();
 
-const kittySchema = new mongoose.Schema({
-  name: String
-});
+export class NeilDB {
 
-// var Kitten = mongoose.model('Kitten', kittySchema);
+  url: string;
+  mongoClient?: MongoClient;
+  db?: Db;
 
-const Cat = mongoose.model('Cat', kittySchema);
+  constructor(url: string) {
+      this.url = url;
+  }
 
-const kitty = new Cat({ name: 'Zildjian2' });
-kitty.save().then(() => console.log('meow'));
+  async connect() {
+      const options: MongoClientOptions = {
+          ssl: true,
+          socketTimeoutMS: 1000,
+          useNewUrlParser: true,
+      };
+
+      return new Promise((resolve, reject) => {
+          MongoClient.connect(this.url, options, (err, client) => {
+              if (err) {
+                  reject(err);
+              } else {
+                  this.mongoClient = client;
+                  this.db = client.db();
+                  resolve(this.db);
+              }
+          });
+      });
+  }
+
+  async disconnect() {
+      this.mongoClient!.close();
+  }
+
+  async getCollection(collectionName: string) {
+    const options: DbCollectionOptions = {
+        readPreference: ReadPreference.PRIMARY_PREFERRED,
+    };
+
+    return this.db!.collection(collectionName, options, () => {});
+  }
+}
+
+// const db = new NeilDB(process.env.MongoURL);
+
+// const main = async () => {
+//   const mongoUrl = process.env.MONGO_URL || 'mongodb://localhost:27017';
+
+//   const ndb = new NeilDB(mongoUrl);
+//   await ndb.connect();
+
+//   const col = await ndb.getCollection('subscribers')
+
+//   await col.insertOne({ email: 'test1', createdAt: new Date(), confirmed: false})
+
+//   const users = await col.find({});
+
+//   await users.forEach((user: any) => {
+//     console.log("user is", user);
+//   });
+
+//   await ndb.disconnect();
+// }
+
+// main().then();
+
+
+// const test = async () => {
+//   await.
+
+// }
+// db.connect().then((db) => {
+//   db.getCollection('subscribers')
+// })
+
+
+
+// class NeilDB {
+
+
+
+
+
+
+
+// }
+// const options: MongoClientOptions = {
+//   ssl: true,
+//   socketTimeoutMS: 1000,
+//   useNewUrlParser: true,
+// };
+
+// return new Promise((resolve, reject) => {
+//   MongoClient.connect(this.url, options, (err, client) => {
+//       if (err) {
+//           reject(err);
+//       } else {
+//           this.mongoClient = client;
+//           this.db = client.db();
+//           resolve(this.db);
+//       }
+//   });
+// });
+
+
+
+
+
+
+// mongoose.connect(process.env.MONGO_URL, {useNewUrlParser: true});
+
+// const SubscriberSchema = new mongoose.Schema({
+//   email: String,
+//   createdAt: Date,
+//   confirmed: Boolean,
+// });
+
+// const Subscriber = mongoose.model('Subscriber', subscriberSchema);
+
+// var db = mongoose.connection;
+// db.on('error', console.error.bind(console, 'connection error:'));
+// db.once('open', function() {
+//   console.log('were connected!')
+
+//   User.find(function (err, users) {
+//     if (err) return console.error(err);
+//     console.log("users is", users);
+//   })
+
+//   // const user2 = new User({ email: 'test', createdAt: new Date(), confirmed: false});
+//   // user2.save().then((err) => console.log('user saved', user2, err));
+
+
+// });
+
+// // const user2 = new User({ email: 'test', createdAt: new Date(), confirmed: false});
+// // user2.save().then((err) => console.log('user saved', user2, err));
 
--- src/mailer.ts
+++ src/mailer.ts
@@ -3,6 +3,8 @@ import mailgunTransport from 'nodemailer-mailgun-transport'
 import dotenv from 'dotenv';
 dotenv.config();
 
+import { NeilDB } from './db';
+
 // const transporter = nodemailer.createTransport({
 //   host: process.env.MAIL_HOST || '',
 //   port: parseInt(process.env.MAIL_PORT, 10) || 587,
@@ -12,16 +14,55 @@ dotenv.config();
 //   },
 // });
 
-// Configure transport options
 const mailgunOptions = {
   auth: {
     api_key: process.env.MAILGUN_API_KEY,
     domain: process.env.MAILGUN_URL,
-  }
-}
+  },
+};
 
 const xport = mailgunTransport(mailgunOptions);
 const mailgun = nodemailer.createTransport(xport);
+
+const sendMail = async () => {
+  const mongoUrl = process.env.MONGO_URL || 'mongodb://localhost:27017';
+
+  const ndb = new NeilDB(mongoUrl);
+  await ndb.connect();
+
+  const col = await ndb.getCollection('subscribers')
+
+  await col.insertOne({ email: 'test1', createdAt: new Date(), confirmed: false})
+
+  const users = await col.find({});
+
+  await users.forEach((user: any) => {
+    const message = {
+      from: 'neil@electricneil.com',
+      to: user.email,
+      subject: 'mailgun test msg✔',
+      text: 'NEIL!',
+      html: `<p>hello ${user.email}, your mongo id is ${user._id}</p>`
+    };
+
+
+    mailgun.sendMail(message, (err: any, info: any) => {
+      if (err) {
+        console.log('Error occurred. ' + err.message);
+        return process.exit(1);
+      }
+      console.log("err, info", err, info)
+      console.log('Message sent: %s', info.messageId);
+      // Preview only available when sending through an Ethereal account
+      console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+    });
+  });
+
+  await ndb.disconnect();
+}
+
+sendMail();
+
 
 
 // const gmail = nodemailer.createTransport({
@@ -32,40 +73,39 @@ const mailgun = nodemailer.createTransport(xport);
 //        },
 // });
 
+// const message = {
+//   from: 'neil@electricneil.com',
+//   to: 'joshstillman@gmail.com',
+//   subject: 'mailgun test msg✔',
+//   text: 'NEIL!',
+//   html: '<p><b>NEIL</b> to myself!</p>'
+// };
 
-const message = {
-  from: 'neil@electricneil.com',
-  to: 'joshstillman@gmail.com',
-  subject: 'mailgun test msg✔',
-  text: 'NEIL!',
-  html: '<p><b>NEIL</b> to myself!</p>'
-};
-
-mailgun.sendMail(message, (err: any, info: any) => {
-    if (err) {
-      console.log('Error occurred. ' + err.message);
-      return process.exit(1);
-    }
-    console.log("err, info", err, info)
-    console.log('Message sent: %s', info.messageId);
-    // Preview only available when sending through an Ethereal account
-    console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
-});
-
-
-
-
-// transporter.sendMail(message, (err, info) => {
+// mailgun.sendMail(message, (err: any, info: any) => {
 //     if (err) {
-//         console.log('Error occurred. ' + err.message);
+//       console.log('Error occurred. ' + err.message);
 //       return process.exit(1);
 //     }
-
+//     console.log("err, info", err, info)
 //     console.log('Message sent: %s', info.messageId);
 //     // Preview only available when sending through an Ethereal account
 //     console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
 // });
-// gmail.sendMail(message, (err, info) => {
+
+
+
+
+// // transporter.sendMail(message, (err, info) => {
+// //     if (err) {
+// //         console.log('Error occurred. ' + err.message);
+// //       return process.exit(1);
+// //     }
+
+// //     console.log('Message sent: %s', info.messageId);
+// //     // Preview only available when sending through an Ethereal account
+// //     console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+// // });
+// // gmail.sendMail(message, (err, info) => {
 //     if (err) {
 //         console.log('Error occurred. ' + err.message);
 //       return process.exit(1);
