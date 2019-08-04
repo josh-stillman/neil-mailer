abb28cabcf6db4d260a061337bd6e236bd9ea8d2
Josh Stillman <josh.stillman@giantmachines.com>
2019-08-03 19:15:26 -0700

    add skeleton email previewer

---
+++ src/emailPreview/index.ts
@@ -0,0 +1,20 @@
+// import express from 'express';
+// import path from 'path';
+
+// const app = express();
+
+// app.set('view engine', 'ejs');
+// // app.set('views', path.join(__dirname, '../..', '/emails/schedule-appointment'));
+
+// const LOCALS = {
+//   unsubscribeLink: 'https://www.electricneil.com/unsubscribe'
+// };
+
+// app.get('/favicon.ico', (req: any, res: any) => res.status(204));
+
+// app.get('/:templateId', (req: any, res: any) => {
+//   app.set('views', path.join(__dirname, '../..', `/emails/${req.params.templateId}`));
+//   res.render('html', LOCALS);
+// });
+
+// app.listen(4000, () => console.log('Email Preview Server running on port 4000!'));
