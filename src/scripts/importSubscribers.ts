5bdd6611c978a91c02a719cf3e2fd4529010e273
Josh Stillman <josh.stillman@giantmachines.com>
2019-08-05 10:09:04 -0400

    update csv import script

---
+++ src/scripts/importSubscribers.ts
@@ -0,0 +1,32 @@
+import papaparse from 'papaparse';
+import fs from 'fs';
+import dotenv from 'dotenv';
+dotenv.config();
+
+import { NeilDB } from '../db';
+
+const myFile = fs.createReadStream(process.argv[2]);
+
+papaparse.parse(myFile, {
+  header: true,
+  skipEmptyLines: 'greedy',
+  complete: (results: any) => {
+    importSubscribers(results.data);
+  }
+});
+
+
+const importSubscribers = async (rows: any) => {
+  const mongoUrl = process.env.MONGO_URL || 'mongodb://localhost:27017/neil';
+  // console.log("rows are,", rows)
+  const ndb = new NeilDB(mongoUrl);
+  await ndb.connect();
+
+  const col = await ndb.getCollection('subscribers')
+
+  const newSubscribers = rows.map((row: any) => ({email: row.email, name: row.name, ...row.phoneNumber ? {phoneNumber: row.phoneNumber} : {}, createdAt: new Date().toISOString(), confirmed: true}))
+  // console.log("newSubscribers are", newSubscribers)
+  await col.insertMany(newSubscribers, { ordered: false })
+
+  await ndb.disconnect();
+}
