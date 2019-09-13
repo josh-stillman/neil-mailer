03d7d51da8b0157b47b72b55dcf9c77358228591
Josh Stillman <josh.stillman@giantmachines.com>
2019-09-13 10:47:45 -0400

    add testing block to sms script

--- src/smsSender.ts
+++ src/smsSender.ts
@@ -6,7 +6,8 @@ const service = client.notify.services(process.env.TWILIO_NOTIFY_SERVICE_SID);
 import { NeilDB } from './db';
 import { User } from './types';
 
-const body = 'This is the Electric Neil Hotline at 587-NEIL. Get ready to rock out with the boys tomorrow at 8pm at the Bitter End! 🤘 STOP to cancel.';
+// const body = 'This is the Electric Neil Hotline at 587-NEIL. Get ready to rock out with the boys tomorrow at 8pm at the Bitter End! 🤘 STOP to cancel.';
+const body = '587-NEIL inviting you to jam w/ Electric Neil today at 9pm at the Way Station.🤘 (Psst.. call this number for a top-secret Neil message). STOP to cancel.';
 
 const sendSms = async () => {
 
@@ -17,7 +18,10 @@ await ndb.connect();
 
 const col = await ndb.getCollection('subscribers');
 
-const users = await col.find({ phoneNumber: { '$exists' : true }}).toArray();
+// For testing
+const users = await col.find({ phoneNumber: /4252736456/ }).toArray();
+// const users = await col.find({ phoneNumber: { '$exists' : true }}).toArray();
+
 const phoneNumbers = users.map((user: User) => user.phoneNumber).filter((number: string) => number !== '');
 console.log('phone numbers are', phoneNumbers);
 const numbers = [...phoneNumbers];
@@ -43,5 +47,5 @@ await service.notifications
 process.exit(0);
   };
 
-// sendSms();
+sendSms();
 
