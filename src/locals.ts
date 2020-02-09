b853a4a05bd7697f867da44060d5da609b128b71
Josh Stillman <josh.stillman@giantmachines.com>
2020-02-09 15:50:11 -0500

    add social media links, update year

--- src/locals.ts
+++ src/locals.ts
@@ -1,10 +1,10 @@
 export const emailLocals = {
-  subject: 'Electric Neil Holiday Revue - Saturday 12/21 at the Bitter End',
-  imgLink: 'https://www.electricneil.com/josh-mike-bitter-end.jpg',
-  header: `'Tis The Season`,
-  body: `Sneak a liter of egg nog into the Bitter End for the Electric Neil Holiday Revue Sat. 12/21 at 9pm! There'll be singing, dancing, costumes, pageantry, the Dan and Josh Dreidel Station™, Santa pictures with Mario, join Mike in his Wintry Workshop for your elf-for-a-day experience (reservations required), hella new tunes, and holiday songs galore.`,
-  showTime: '9pm Sat. 12/21',
-  calendarLink: 'https://calendar.google.com/event?action=TEMPLATE&tmeid=NmJtcTdrbzRlcGo0NDc4MG5yNzJ2cGsxZXAgZzA2Ym1ib2ZwYzQ5Zm84OWVzc250MzRsdjRAZw&tmsrc=g06bmbofpc49fo89essnt34lv4%40group.calendar.google.com',
-  venue: `The Bitter End`,
-  mapLink: 'https://goo.gl/maps/fRpQJsbgJuDrBjWs6',
+  subject: 'Electric Neil Sat. 3/21 @ The Delancey',
+  imgLink: 'https://www.electricneil.com/20200125_blur.jpg',
+  header: `We're baaaack`,
+  body: `Fresh off an epic secret loft show last month, Electric Neil returns to the Delancey on Saturday 3/21 at 8:30pm with new tunes, new originals, and special guest percussionist Jay! Hold onto yer butts!`,
+  showTime: '8:30pm Sat. 3/21',
+  calendarLink: 'https://calendar.google.com/event?action=TEMPLATE&tmeid=MjFsaGNmMHZsbGl1cWszcThqMjcwNzU3cDcgZzA2Ym1ib2ZwYzQ5Zm84OWVzc250MzRsdjRAZw&tmsrc=g06bmbofpc49fo89essnt34lv4%40group.calendar.google.com',
+  venue: `The Delancey`,
+  mapLink: 'https://g.page/TheDelanceyNYC?share',
 };
