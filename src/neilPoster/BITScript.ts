45ec1230fc30d01c4e34629adb112149055dd7d7
Josh Stillman <josh.stillman@giantmachines.com>
2020-02-09 15:12:53 -0500

    add initial posting scripts

---
+++ src/neilPoster/BITScript.ts
@@ -0,0 +1,36 @@
+import puppeteer from 'puppeteer'
+import dotenv from 'dotenv';
+dotenv.config();
+
+const post = async () => {
+
+  const browser = await puppeteer.launch({
+    args: ["--no-sandbox"],
+    headless: false
+  })
+
+  const page = await browser.newPage()
+  //TODO - env var.
+  await page.goto(process.env.BIT_URL)
+
+  await page.click('button.Button__Btn-sc-3cqxe3-0.caezgZ')
+  await page.type('#loginEmail', 'joshstillman@gmail.com')
+
+  //TODO - env vars
+  await page.type('#loginPassword', 'wearetheNeil1!')
+  await page.click('[type="submit"]');
+  await page.waitForSelector('textarea.Text__ArtistPostsMessageBoxText-sc-1lubkui-0');
+  await page.type('textarea.Text__ArtistPostsMessageBoxText-sc-1lubkui-0', 'hello world2');
+
+  const checkboxes = await page.$$('.Checkbox__StyledCheckbox-sc-3aet-0')
+
+  //b, f, t
+  await checkboxes[0].click()
+  await checkboxes[2].click()
+
+  await page.click('button.Button__Btn-sc-3cqxe3-0.iCpAnl.MainButton__StyledButton-f1zfm1-0.gTXneI')
+  await page.waitForSelector('button.Button__Btn-sc-3cqxe3-0.bdFMaA.DialogButton__StyledButton-q30yea-0.chCmbC');
+  await page.click('button.Button__Btn-sc-3cqxe3-0.bdFMaA.DialogButton__StyledButton-q30yea-0.chCmbC')
+
+}
+post();
---
+++ src/neilPoster/IGScript.ts
@@ -0,0 +1,75 @@
+import puppeteer from 'puppeteer'
+import devices from 'puppeteer/DeviceDescriptors';
+import dotenv from 'dotenv';
+dotenv.config();
+
+// const puppeteer = require('puppeteer')
+// const devices = require('puppeteer/DeviceDescriptors')
+const iPhonex = devices['iPhone X'];
+
+// TODO: file should be an argument here, fetch before promise.all the posting.
+//TODO: delete file from disk after post.
+// TODO: figure out where to
+const post = async () => {
+
+  const browser = await puppeteer.launch({
+    args: ["--no-sandbox"],
+    headless: false
+  })
+
+  const page = await browser.newPage();
+  await page.emulate(iPhonex);
+
+  //TODO - env var.
+  await page.goto(process.env.IG_URL)
+  await page.waitForSelector('button.sqdOP.L3NKy.y3zKF');
+  await page.click('button.sqdOP.L3NKy.y3zKF')
+
+  await page.waitForSelector('input[name="username"]');
+  await page.type('[name="username"]', 'electric.neil', {delay: 10})
+  await page.waitForSelector('input[name="password"]');
+  await page.type('[name="password"]', 'wearetheNeil1!', {delay: 10})
+  await page.type('[name="username"]', 'electric.neil', {delay: 10})
+  await page.click('[type="submit"]');
+  //remember
+  await page.waitForSelector('button.GAMXX');
+  if (page.$('button.GAMXX')) {
+
+    await page.click('button.GAMXX')
+  }
+  // add to home screen
+  await page.waitForSelector('button.aOOlW:last-of-type');
+  if (page.$('button.aOOlW:last-of-type')) {
+    await page.click('button.aOOlW:last-of-type')
+  }
+
+  // await page.click('span[aria-label="New Post"]')
+
+  // const request = require('request-promise');
+
+  // const fileData = await request({
+  //     uri: 'https://www.electricneil.com/dan-noir.jpg',
+  //     encoding: 'binary'
+  // });
+
+  // fs.writeFileSync('./dan8.jpg', {encoding: 'binary'} 'fileData');
+
+
+  const [fileChooser] = await Promise.all([
+    page.waitForFileChooser(),
+    page.click('span[aria-label="New Post"]'), // some button that triggers file selection
+  ]);
+
+  await fileChooser.accept(['./dan8.jpg'])
+  await page.waitForSelector('button.UP43G');
+  await page.click('button.UP43G')
+
+  await page.waitForSelector('textarea');
+
+
+  await page.type('textarea', "Neil gettin' funky!")
+  await page.click('button.UP43G')
+
+  //TODO - env vars
+}
+post();
