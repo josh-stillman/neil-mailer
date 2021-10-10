19aa5da0f168cb649ec0a1bd33e0301dafb3bbb2
Josh Stillman <josh.stillman@giantmachines.com>
2021-10-10 19:28:39 -0400

    add neil's back email

--- src/locals.ts
+++ src/locals.ts
@@ -1,10 +1,10 @@
 export const emailLocals = {
-  subject: 'Electric Neil Sat. 3/21 @ The Delancey',
-  imgLink: 'https://www.electricneil.com/20200125_blur.jpg',
-  header: `We're baaaack`,
-  body: `Fresh off an epic secret loft show last month, Electric Neil returns to the Delancey on Saturday 3/21 at 8:30pm with new tunes, new originals, and special guest percussionist Jay! Hold onto yer butts!`,
-  showTime: '8:30pm Sat. 3/21',
-  calendarLink: 'https://calendar.google.com/event?action=TEMPLATE&tmeid=MjFsaGNmMHZsbGl1cWszcThqMjcwNzU3cDcgZzA2Ym1ib2ZwYzQ5Zm84OWVzc250MzRsdjRAZw&tmsrc=g06bmbofpc49fo89essnt34lv4%40group.calendar.google.com',
-  venue: `The Delancey`,
-  mapLink: 'https://g.page/TheDelanceyNYC?share',
+  subject: 'Electric Neil Saturday 8:30pm @ Freddys (New Set Time)',
+  imgLink: 'https://www.electricneil.com/set-time.jpg',
+  header: `New Set Time`,
+  body: `The night just keeps getting bigger!  Now Electric Neil will be kicking off the night at 8:30pm. After the Neil, stick around for the jammy sounds of Lava Party, a dance party from DJ C-PROD-G, and a newly added set from James Brown's bassist, the legendary Fred Thomas! We're gonna have a funky good time!`,
+  showTime: '8:30pm Sat. 9/25',
+  calendarLink: 'https://calendar.google.com/event?action=TEMPLATE&tmeid=MG91dnFpcjJxajVtcG80aXRiczcydHZzYnQgZzA2Ym1ib2ZwYzQ5Zm84OWVzc250MzRsdjRAZw&tmsrc=g06bmbofpc49fo89essnt34lv4%40group.calendar.google.com',
+  venue: `Freddy's`,
+  mapLink: 'https://g.page/freddysbrklyn?share',
 };
