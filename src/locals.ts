c03fca450333ce6e6a9323a65166fba94d425b8c
Josh Stillman <josh.stillman@giantmachines.com>
2022-10-26 13:24:15 -0400

    update locals

--- src/locals.ts
+++ src/locals.ts
@@ -1,10 +1,10 @@
 export const emailLocals = {
-  subject: 'Electric Neil Saturday 8:30pm @ Freddys (New Set Time)',
-  imgLink: 'https://www.electricneil.com/set-time.jpg',
-  header: `New Set Time`,
-  body: `The night just keeps getting bigger!  Now Electric Neil will be kicking off the night at 8:30pm. After the Neil, stick around for the jammy sounds of Lava Party, a dance party from DJ C-PROD-G, and a newly added set from James Brown's bassist, the legendary Fred Thomas! We're gonna have a funky good time!`,
-  showTime: '8:30pm Sat. 9/25',
-  calendarLink: 'https://calendar.google.com/event?action=TEMPLATE&tmeid=MG91dnFpcjJxajVtcG80aXRiczcydHZzYnQgZzA2Ym1ib2ZwYzQ5Zm84OWVzc250MzRsdjRAZw&tmsrc=g06bmbofpc49fo89essnt34lv4%40group.calendar.google.com',
-  venue: `Freddy's`,
-  mapLink: 'https://g.page/freddysbrklyn?share',
+  subject: 'Hot Neil Summer, Sat. 8/13 @ Young Ethels',
+  imgLink: 'https://www.electricneil.com/hot-neil-summer.jpg',
+  header: `Cool off to the hot sounds of Electric Neil & Friends!`,
+  body: `Electric Neil & Friends are back and we've got the cure for your summertime blues! We'll be posting up all night at Young Ethel's on Saturday 8/13, serving up heaps of hot new jams.  Hope to see you there for another epic Neil party!  It's gonna be a scorcher!`,
+  showTime: '8pm Sat. 8/13',
+  calendarLink: 'https://calendar.google.com/event?action=TEMPLATE&tmeid=NTJwbnFxaWxoZWVxMXFva2M5YjE0aDEwMDEgZzA2Ym1ib2ZwYzQ5Zm84OWVzc250MzRsdjRAZw&tmsrc=g06bmbofpc49fo89essnt34lv4%40group.calendar.google.com',
+  venue: `Young Ethel's`,
+  mapLink: 'https://g.page/youngethels?share',
 };
